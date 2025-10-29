b88c68e3df6f624b2553ad65e2e2ae3a59259a28
Hamza <hamza@xoio.us>
2025-10-29 15:31:44 +0000

    add move.nvim plugin

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -12,4 +12,18 @@ return {
 	{ "ethanholz/nvim-lastplace", opts = {} },
 	{ "echasnovski/mini.surround", event = "VeryLazy", version = false, opts = {} },
 	{ "altermo/ultimate-autopair.nvim", event = { "InsertEnter", "CmdlineEnter" }, branch = "v0.6", opts = {} },
+	{
+		"fedepujol/move.nvim",
+		keys = {
+			-- Normal Mode
+			{ "<A-down>", ":MoveLine(1)<CR>", desc = "Move Line Up" },
+			{ "<A-up>", ":MoveLine(-1)<CR>", desc = "Move Line Down" },
+			-- Visual Mode
+			{ "<A-down>", ":MoveBlock(1)<CR>", mode = { "v" }, desc = "Move Block Up" },
+			{ "<A-up>", ":MoveBlock(-1)<CR>", mode = { "v" }, desc = "Move Block Down" },
+		},
+		opts = {
+			-- Config here
+		},
+	},
 }
