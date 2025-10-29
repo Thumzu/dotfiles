bc692f6f2006a11588511947c9312333824faa09
Hamza <hamza@xoio.us>
2025-10-29 18:00:53 +0000

    make nvim catppuccin transparent

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -5,20 +5,10 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd.colorscheme("catppuccin-macchiato")
+			-- vim.cmd.colorscheme("gruber-darker")
 		end,
 	},
-	{
-		"vague2k/vague.nvim",
-		opts = {
-			on_highlights = function(hl, _)
-				hl.StatusLine.bg = "NONE"
-				hl.StatusLineNC.bg = "NONE"
-			end,
-		},
-	},
-	{ "catppuccin/nvim", name = "catppuccin", tag = "v1.11.0" }, -- update when TS is fixed
-	{ "folke/tokyonight.nvim", opts = { style = "night" } },
-	{ "rose-pine/neovim", name = "rose-pine" },
-	{ "loctvl842/monokai-pro.nvim" },
-	{ "sainnhe/gruvbox-material" },
+
+	{ "catppuccin/nvim", opts = {transparent_background = true, float = {transparent = true}}, name = "catppuccin"},
+	{ "blazkowolf/gruber-darker.nvim" }
 }
