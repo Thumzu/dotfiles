908f0ed4721387de85e249e448efe4cd25b81695
Hamza <hamza@xoio.us>
2025-12-02 00:57:42 +0000

    change to mocha theme

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -4,7 +4,8 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("catppuccin-macchiato")
+			vim.cmd.colorscheme("catppuccin-mocha")
+			-- vim.cmd.colorscheme("catppuccin-macchiato")
 			-- vim.cmd.colorscheme("gruber-darker")
 		end,
 	},
