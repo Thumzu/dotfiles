79536f713e7fcd46f60079ecfd578e666af6f210
Hamza <hamza@xoio.us>
2025-12-02 00:59:02 +0000

    fix transaparency and line spacing

--- .config/nvim/lua/core/neovide.lua
+++ .config/nvim/lua/core/neovide.lua
@@ -1,6 +1,6 @@
 vim.o.guifont = "JetbrainsMono Nerd Font:h15:#h-none"
 
-vim.opt.linespace = 10
+vim.opt.linespace = 5
 
 vim.g.neovide_cursor_animation_length = 0.05
 vim.g.neovide_cursor_trail_size = 0.5
@@ -8,7 +8,16 @@ vim.g.neovide_cursor_trail_size = 0.5
 vim.g.neovide_input_macos_option_key_is_meta = "only_left"
 vim.g.neovide_hide_mouse_when_typing = true
 
-vim.g.neovide_show_border = true
+-- Helper function for transparency formatting
+local alpha = function()
+  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+end
+-- g:neovide_opacity should be 0 if you want to unify transparency of content and title bar.
+vim.g.neovide_opacity = 0.0
+vim.g.transparency = 0.8
+vim.g.neovide_background_color = "#24273a" .. alpha()
+vim.g.neovide_window_blurred = true
+
 
 -- Allow clipboard copy paste in neovim (MacOS)
 vim.keymap.set('v', '<D-c>', '"+y')
