4c4420d7af05b8d64282b2d827a0354be3f29bd5
Hamza <hamza@xoio.us>
2025-12-09 21:50:29 +0000

    commented out broken transparency code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 require("core")
 
 -- Make background transparent
-vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none", bg = "none" })
-vim.api.nvim_set_hl(0, "NonText", { ctermbg = "none", bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none", bg = "none" })
+-- vim.api.nvim_set_hl(0, "NonText", { ctermbg = "none", bg = "none" })
