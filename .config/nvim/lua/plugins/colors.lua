d1bda98ad5d4986b3a9038de9016bc934b0976d3
Hamza <hamza@xoio.us>
2026-05-10 23:14:47 +0100

    add new color themes

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -4,12 +4,27 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
-			-- vim.cmd.colorscheme("catppuccin-macchiato")
+ 			-- vim.cmd.colorscheme("catppuccin-mocha")
 			-- vim.cmd.colorscheme("gruber-darker")
+			-- vim.cmd.colorscheme("ghostty-default-style-dark")
+			vim.cmd.colorscheme("custom")
+			-- Apply your custom modifications
+			-- vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+			-- vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
 		end,
 	},
 
-	{ "catppuccin/nvim", opts = {transparent_background = true, float = {transparent = true}}, name = "catppuccin"},
-	{ "blazkowolf/gruber-darker.nvim" }
+	-- { "catppuccin/nvim", opts = {transparent_background = true, float = {transparent = true}}, name = "catppuccin"},
+	{ "blazkowolf/gruber-darker.nvim" },
+	{
+		"nkxxll/ghostty-default-style-dark.nvim",
+		opts = {
+			on_highlights = function(hl, _)
+				hl.StatusLine.bg = "NONE"
+				hl.StatusLineNC.bg = "NONE"
+			end,
+		},
+	},
 }
+
+
