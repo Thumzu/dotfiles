054a55437b647b0404d24388641392e467d82858
Hamza <hamza@xoio.us>
2026-05-30 23:34:53 +0100

    feat: update to use nvim v0.12 statusline with new diagnostics

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -10,7 +10,8 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 -- Set GUI options
--- vim.opt.cursorline = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
@@ -18,17 +19,18 @@ vim.opt.splitright = true
 vim.opt.mousemoveevent = true
 vim.opt.pumheight = 12 -- limit completion items (lsp)
 vim.opt.laststatus = 3 -- only one statusbar per window
--- vim.opt.statusline = "%=%t%="
-vim.o.winborder = "rounded"
 
--- Centre screen after commands
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-f>", "<C-f>zz")
-vim.keymap.set("n", "<C-b>", "<C-b>zz")
-
-vim.keymap.set("n", "G", "Gzz")
-
+-- Set default status with git branch
+function _G.get_branch_from_git()
+  local branch
+  local gitsigns = vim.b.gitsigns_status_dict
+  -- TODO: fix so that you don't have to move for branch to show
+  if gitsigns and gitsigns.head and gitsigns.head ~= '' then
+    branch = gitsigns.head
+  end
+  return (branch and branch ~= "") and ("Git:" .. branch .. "        ") or ""
+end
+vim.opt.statusline = "%<%f %h%w%m%r %{% v:lua.require('vim._core.util').term_exitcode() %}%=%{% luaeval('(package.loaded[''vim.ui''] and vim.api.nvim_get_current_win() == tonumber(vim.g.actual_curwin or -1) and vim.ui.progress_status()) or '''' ')%}%{% &showcmdloc == 'statusline' ? '%-10 .S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &busy > 0 ? '◐ ' : '' %}%{v:lua.get_branch_from_git()}%{% luaeval('(package.loaded[''vim.diagnostic''] and next(vim.diagnostic.count()) and vim.diagnostic.status() .. '' '') or '''' ') %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l, %c%V%) %P' : &rulerformat ) : '' %}"
 
 -- Misc
 vim.opt.spelllang = "en_gb"
@@ -37,8 +39,13 @@ vim.opt.title = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- Clear search highlights with Esc in normal mode (DOESN'T WORK IN A CLEAN NVIM)
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- Centre screen after commands
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+
+vim.keymap.set("n", "G", "Gzz")
 
 -- Neovide
 if vim.g.neovide then
